1ac5d3a53c42d7a3cb1a4012c3adfc1272d8672f
DanielSanuwich <106105378+danielsanuwich@users.noreply.github.com>
2023-04-21 06:44:23 +0200

    bugfixed tile and added enum for card

--- maven-roborally/src/main/java/app/Model/cards/Card.java
+++ maven-roborally/src/main/java/app/Model/cards/Card.java
@@ -1,12 +1,32 @@
-package cards;
+package app.Model.cards;
 
 public class Card {
-	private String attribute;
-	public Card(String attribute) {
-		this.attribute = attribute;
-	}
-	
-	public void executeCard() {
-		
-	}
+    // The attribute that defines the card's behavior
+    private final String attribute;
+
+    // Public static final instances of the Card class, representing different card types
+    // These instances can be used as a factory to create cards with predefined attributes
+    public static final Card MOVE_ONE = new Card("MOVE ONE");
+    public static final Card MOVE_TWO = new Card("MOVE TWO");
+    public static final Card MOVE_THREE = new Card("MOVE THREE");
+    public static final Card BACK_UP = new Card("BACK UP");
+    public static final Card ROTATE_RIGHT = new Card("ROTATE RIGHT");
+    public static final Card ROTATE_LEFT = new Card("ROTATE LEFT");
+    public static final Card U_TURN = new Card("U TURN");
+
+    // Private constructor to restrict instantiation from outside the class
+    // Ensures that only the defined card types can be created
+    private Card(String attribute) {
+        this.attribute = attribute;
+    }
+
+    // Getter method to access the card's attribute
+    public String getAttribute() {
+        return attribute;
+    }
+
+    // Method to execute the action associated with the card's attribute
+    public void executeCard() {
+        // Perform the action associated with the card's attribute
+    }
 }
