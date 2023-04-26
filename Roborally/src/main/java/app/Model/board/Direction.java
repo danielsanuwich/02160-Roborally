83245424a6e5f6249ee65d7b7533705f389c8c8b
DanielSanuwich <106105378+danielsanuwich@users.noreply.github.com>
2023-04-26 11:34:57 +0200

    new test for direction

--- Roborally/src/main/java/app/Model/board/Direction.java
+++ Roborally/src/main/java/app/Model/board/Direction.java
@@ -1,36 +1,28 @@
 package app.Model.board;
 
-public class Main {
+public class Direction {
+    private int direction;
 
-    public static void main(String[] args) {
-        // create a new Direction object with initial direction 0
-        Direction direction = new Direction(0);
-
-        try {
-            // test the initial direction value
-            assert direction.getDirection() == 0;
-
-            // test setting the direction value to a positive number
-            direction.setDirection(3);
-            assert direction.getDirection() == 3;
-
-            // test setting the direction value to a negative number
-            direction.setDirection(-2);
-            assert direction.getDirection() == 4;
-
-            // test setting the direction value to a large positive number
-            direction.setDirection(13);
-            assert direction.getDirection() == 1;
-
-            // test setting the direction value to a large negative number
-            direction.setDirection(-17);
-            assert direction.getDirection() == 5;
-
-            // print a message indicating that all tests passed
-            System.out.println("All tests passed!");
-        } catch (AssertionError e) {
-            // print a message indicating that the test did not pass
-            System.out.println("Test not passed: " + e.getMessage());
+    // direction constructor
+    public Direction(int initDirection) {
+        if (initDirection < 0) {
+            direction = 6 + (initDirection % 6);
+        } else {
+            direction = initDirection % 6;
         }
+    }
+
+    // direction setter
+    public void setDirection(int initDirection) {
+        if (initDirection < 0) {
+            direction = 6 + (initDirection % 6);
+        } else {
+            direction = initDirection % 6;
+        }
+    }
+
+    // direction getter
+    public int getDirection() {
+        return this.direction;
     }
 }
--- Roborally/src/main/java/app/main.java
+++ Roborally/src/main/java/app/main.java
@@ -1,34 +1,36 @@
-package app;
+package app.Model.board;
 
-import app.Model.board.Direction;
-
-public class main {
+public class Main {
 
     public static void main(String[] args) {
-        // create a new direction object with initial direction 0
-        Direction dir = new Direction(0);
+        // create a new Direction object with initial direction 0
+        Direction direction = new Direction(0);
 
-        // test the initial direction value
-        assert dir.getDirection() == 0;
+        try {
+            // test the initial direction value
+            assert direction.getDirection() == 0;
 
-        // test setting the direction value to a positive number
-        dir.setDirection(3);
-        assert dir.getDirection() == 3;
+            // test setting the direction value to a positive number
+            direction.setDirection(3);
+            assert direction.getDirection() == 3;
 
-        // test setting the direction value to a negative number
-        dir.setDirection(-2);
-        assert dir.getDirection() == 4;
+            // test setting the direction value to a negative number
+            direction.setDirection(-2);
+            assert direction.getDirection() == 4;
 
-        // test setting the direction value to a large positive number
-        dir.setDirection(13);
-        assert dir.getDirection() == 1;
+            // test setting the direction value to a large positive number
+            direction.setDirection(13);
+            assert direction.getDirection() == 1;
 
-        // test setting the direction value to a large negative number
-        dir.setDirection(-17);
-        assert dir.getDirection() == 5;
+            // test setting the direction value to a large negative number
+            direction.setDirection(-17);
+            assert direction.getDirection() == 5;
 
-        // print a message indicating that all tests passed
-        System.out.println("All tests passed!");
+            // print a message indicating that all tests passed
+            System.out.println("All tests passed!");
+        } catch (AssertionError e) {
+            // print a message indicating that the test did not pass
+            System.out.println("Test not passed: " + e.getMessage());
+        }
     }
-
 }
