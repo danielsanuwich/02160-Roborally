6371bc220b3addc115b552a6a9d9c2649715bde1
TomasHarmer <tharmer@a-is.com>
2023-04-26 14:19:02 +0200

    Implemented more unit tests

--- Roborally/src/test/java/app/Model/cards/ProgramCardTest.java
+++ Roborally/src/test/java/app/Model/cards/ProgramCardTest.java
@@ -1,8 +1,38 @@
 package app.Model.cards;
 
-/*
- * Program card 
- */
-public class ProgramCardTest{
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+public class ProgramCardTest {
+    
+    @Test
+    public void testConstructorWithTurnAmount() {
+        ProgramCard card = new ProgramCard(3, 0);
+        assertEquals("Uturn", card.getType());
+        assertEquals(3, card.getTurnAmount());
+        assertEquals(0, card.getMoveAmount());
+        
+        card = new ProgramCard(-4, 0);
+        assertEquals("CW4", card.getType());
+        assertEquals(-4, card.getTurnAmount());
+        assertEquals(0, card.getMoveAmount());
+        
+        card = new ProgramCard(8, 0);
+        assertEquals("CCW2", card.getType());
+        assertEquals(8, card.getTurnAmount());
+        assertEquals(0, card.getMoveAmount());
+    }
+    
+    @Test
+    public void testConstructorWithMoveAmount() {
+        ProgramCard card = new ProgramCard(0, 1);
+        assertEquals("Move1", card.getType());
+        assertEquals(0, card.getTurnAmount());
+        assertEquals(1, card.getMoveAmount());
+        
+        card = new ProgramCard(0, -2);
+        assertEquals("Move2", card.getType());
+        assertEquals(0, card.getTurnAmount());
+        assertEquals(-2, card.getMoveAmount());
+    }
 }
