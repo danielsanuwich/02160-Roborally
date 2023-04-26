3edc0cc5959b71b1b56e19b8a2c000c057e986b1
Tim <lim1473@gmail.com>
2023-04-26 15:35:37 +0200

    gameboard updated

--- Roborally/src/main/java/app/Model/board/GameBoard.java
+++ Roborally/src/main/java/app/Model/board/GameBoard.java
@@ -1,5 +1,7 @@
-//Import position
-//import tile
+package app.Model.board;
+
+import app.Model.tiles.StartTile;
+import app.Model.tiles.*;
 
 public class GameBoard {
 
@@ -18,16 +20,11 @@ public class GameBoard {
             for (int j = 0; j < yDim; j++) {
                 this.gameBoard[i][j] = new Tile();
                 this.gameBoard[i][j].setPosition(new Position(i, j));
-                this.gameBoard[i][j].setType(TileType.BASE_TILE);
+                this.gameBoard[i][j].setType("Base");
                 this.gameBoard[i][j].setDirection(0);
             }
         }
 
-
-        
-        // Specify other special tiles
-        // ...
-        
     }
 
     // gets the tile at a certain position
@@ -36,27 +33,28 @@ public class GameBoard {
     }
 
     // computes the distance between any two positions
-    private float distance(Position pos1, Position pos2){
-        return Math.sqrt( Math.pow((pos1.getX()-pos2.getX()),2) + Math.pow((pos1.getY()-pos2.getY()),2) );
+    private float distance(Position pos1, Position pos2) {
+        return (float) Math.hypot(pos1.getX() - pos2.getX(), pos1.getY() - pos2.getY());
     }
 
-
-
     // returns the nearest start tile to any given position
-    private Position nearestStartTile(Position initial){
-        float smallestDistance = Math.abs(xDim + yDim);
-        Position closest; // nearest start tile's position 
+    private Position nearestStartTile(Position initial) {
+        float smallestDistance = Float.MAX_VALUE;
+        Position closest = new Position(0, 0); // nearest start tile's position
 
         // loop to iterate through all positions on the board
-        for(int i = 0; i < gb.xDim){
-            for(int j = 0; j < gb.yDim){
-                if(this.gameBoard[i][j] instanceof StartTile){// if tiletype is startTile 
-                    if(distance(initial, this.gameBoard[i][j].getPosition)<smallestDistance){// if this distance smaller than previous 
-                    // update new smallest distance
-                    }// update new closest start position 
+        for (int i = 0; i < xDim; i++) {
+            for (int j = 0; j < yDim; j++) {
+                if (gameBoard[i][j] instanceof StartTile) {
+                    float dist = distance(initial, gameBoard[i][j].getPosition());
+                    if (dist < smallestDistance) {
+                        smallestDistance = dist;
+                        closest = gameBoard[i][j].getPosition();
+                    }
                 }
             }
         }
+        return closest;
     }
 
 }
