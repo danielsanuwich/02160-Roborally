1ca99836ca5238bb79f3901e5506c8cb4b16dcfa
TomasHarmer <tharmer@a-is.com>
2023-04-29 17:55:42 +0200

    Updated PlayerTurnState

---
+++ Roborally/src/main/java/app/controllers/PlayerTurnState.java
@@ -0,0 +1,29 @@
+// PlayerTurnState -> PlayerExecuteState
+
+
+public class PlayerTurnState extends State{
+
+    // PlayerTurnState Attributes 
+    private String nextStateName;
+    
+    
+    // PlayerTurnState Constructor
+    
+    public PlayerTurnState(String nextStateName){
+        this.nextStateName = nextStateName;
+    }
+    
+    
+    // PlayerTurnState Setters 
+    public void setNextStateName(String nextStateName){
+        this.nextStateName = nextStateName;
+    }
+    
+    
+    // PlayerTurnState Getters 
+
+    public String getNextStateName(){
+        return this.nextStateName;
+    }
+    
+}
